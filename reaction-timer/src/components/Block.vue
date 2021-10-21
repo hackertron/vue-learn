90d338481e40ef9e3763c5a596081ac5a610b303
hackertron <cooljay.gupta@gmail.com>
2021-10-21 16:14:16 +0200

    learning vue

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+      click me
+  </div>
+</template>
+
+<script>
+export default {
+    // props are values/data that can be passed in from the parent component
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        console.log("component mounted")
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+            console.log("delay : ", this.delay)
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime+= 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+            this.$emit('end', this.reactionTime)
+        }
+    }
+    // updated() {
+    //     console.log("component updated")
+    // },
+    // unmounted() {
+    //     console.log("component unmounted")
+    // }
+}
+</script>
+
+<style>
+  .block {
+    width: 400px;
+    border-radius: 20px;
+    background:  #0faf87;;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+
+</style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="results">
+      <p><strong>Results:</strong>{{ score }}</p>
+      <p><strong>Rank:</strong>{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score > 1000) {
+            this.rank = 'You are a noob!'
+        }
+        else if (this.score > 750) {
+            this.rank = 'You are a beginner!'
+        }
+        else if (this.score > 500) {
+            this.rank = 'You are a pro!'
+        }
+        else if (this.score < 250) {
+            this.rank = 'You are a champion!'
+        }
+        else {
+            this.rank = 'You are a god!'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
